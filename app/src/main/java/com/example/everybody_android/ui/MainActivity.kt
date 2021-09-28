0171f32c64807233647685b2d62c2829d965bab2
minjun8946 <minjun8946@daum.net>
2021-09-28 19:05:50 +0900

    main activity

--- app/src/main/java/com/example/everybody_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/everybody_android/ui/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.everybody_android.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.everybody_android.R
+import com.example.everybody_android.base.BaseActivity
+import com.example.everybody_android.databinding.ActivityMainBinding
+import com.example.everybody_android.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding,MainViewModel>() {
+    override val layoutId = R.layout.activity_main
+    override val viewModel: MainViewModel by viewModels()
+
+    override fun init() {
     }
+
+
 }
