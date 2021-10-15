0b18c080f5009aa8f98ab43d9b7569ad9898bfcd
skwntnftkgk <skwntnftkgk@gmail.com>
2021-10-15 22:54:29 +0900

    [Feat] 리팩터링 코드정리

--- app/src/main/java/com/example/everybody_android/ui/camera/ExampleCameraActivity.kt
+++ app/src/main/java/com/example/everybody_android/ui/camera/ExampleCameraActivity.kt
@@ -13,6 +13,7 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.webkit.MimeTypeMap
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -33,6 +34,7 @@ class ExampleCameraActivity : BaseActivity<ActivityCameraBinding, CameraViewMode
     override val layoutId: Int = R.layout.activity_camera
     override val viewModel: CameraViewModel by viewModels()
     private var displayId: Int = -1
+    private var cameraProvider: ProcessCameraProvider? = null
     private var imageCapture: ImageCapture? = null
     private var imageAnalyzer: ImageAnalysis? = null
     private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
@@ -53,9 +55,12 @@ class ExampleCameraActivity : BaseActivity<ActivityCameraBinding, CameraViewMode
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setPermissionCallback(arrayOf(Manifest.permission.CAMERA)) {
-            setUpCamera()
-            cameraExecutor = Executors.newSingleThreadExecutor()
+        binding.pvFinder.post {
+            setPermissionCallback(arrayOf(Manifest.permission.CAMERA)) {
+                displayId = binding.pvFinder.display.displayId
+                cameraExecutor = Executors.newSingleThreadExecutor()
+                cameraSetting()
+            }
         }
     }
 
@@ -70,33 +75,22 @@ class ExampleCameraActivity : BaseActivity<ActivityCameraBinding, CameraViewMode
 
     override fun init() {
         binding.btnSwitch.setOnClickListener {
-            lensFacing = if (CameraSelector.LENS_FACING_FRONT == lensFacing) {
-                CameraSelector.LENS_FACING_BACK
-            } else {
-                CameraSelector.LENS_FACING_FRONT
-            }
-            // Re-bind use cases to update selected camera
-            setUpCamera()
+            lensFacing =
+                if (CameraSelector.LENS_FACING_FRONT == lensFacing) CameraSelector.LENS_FACING_BACK
+                else CameraSelector.LENS_FACING_FRONT
+            bindCameraUseCases()
         }
         binding.btnCapture.setOnClickListener {
             imageCapture?.let { imageCapture ->
-
-                // Create output file to hold the image
                 val photoFile = createFile(getOutputDirectory(), FILENAME, PHOTO_EXTENSION)
-
-                // Setup image capture metadata
                 val metadata = ImageCapture.Metadata().apply {
-
-                    // Mirror image when using the front camera
                     isReversedHorizontal = lensFacing == CameraSelector.LENS_FACING_FRONT
                 }
 
-                // Create output options object which contains file + metadata
                 val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile)
                     .setMetadata(metadata)
                     .build()
 
-                // Setup image capture listener which is triggered after photo has been taken
                 imageCapture.takePicture(
                     outputOptions, cameraExecutor, object : ImageCapture.OnImageSavedCallback {
                         override fun onError(exc: ImageCaptureException) {
@@ -107,17 +101,12 @@ class ExampleCameraActivity : BaseActivity<ActivityCameraBinding, CameraViewMode
                             val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
                             Log.d(TAG, "Photo capture succeeded: $savedUri")
 
-                            // Implicit broadcasts will be ignored for devices running API level >= 24
-                            // so if you only target API level 24+ you can remove this statement
                             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
                                 sendBroadcast(
                                     Intent(Camera.ACTION_NEW_PICTURE, savedUri)
                                 )
                             }
 
-                            // If the folder selected is an external media directory, this is
-                            // unnecessary but otherwise other apps will not be able to access our
-                            // images unless we scan them using [MediaScannerConnection]
                             val mimeType = MimeTypeMap.getSingleton()
                                 .getMimeTypeFromExtension(savedUri.toFile().extension)
                             MediaScannerConnection.scanFile(
@@ -129,11 +118,7 @@ class ExampleCameraActivity : BaseActivity<ActivityCameraBinding, CameraViewMode
                             }
                         }
                     })
-
-                // We can only change the foreground Drawable using API level 23+ API
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-                    // Display flash animation to indicate that photo was captured
                     binding.root.postDelayed({
                         binding.root.foreground = ColorDrawable(Color.WHITE)
                         binding.root.postDelayed(
@@ -146,67 +131,67 @@ class ExampleCameraActivity : BaseActivity<ActivityCameraBinding, CameraViewMode
         displayManager.registerDisplayListener(displayListener, null)
     }
 
-    private fun setUpCamera() {
-        displayId = binding.pvFinder.display.displayId
+
+    private fun cameraSetting() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-            val cameraSelector =
-                CameraSelector.Builder().requireLensFacing(lensFacing).build()
-            val rotation = binding.pvFinder.display.rotation
-            // Preview
-            val preview = Preview.Builder()
-                // We request aspect ratio but no resolution
-                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-                // Set initial target rotation
-                .setTargetRotation(rotation)
-                .build()
-
-            // ImageCapture
-            imageCapture = ImageCapture.Builder()
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                // We request aspect ratio but no resolution to match preview config, but letting
-                // CameraX optimize for whatever specific resolution best fits our use cases
-                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-                // Set initial target rotation, we will have to call this again if rotation changes
-                // during the lifecycle of this use case
-                .setTargetRotation(rotation)
-                .build()
-
-            // ImageAnalysis
-            imageAnalyzer = ImageAnalysis.Builder()
-                // We request aspect ratio but no resolution
-                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-                // Set initial target rotation, we will have to call this again if rotation changes
-                // during the lifecycle of this use case
-                .setTargetRotation(rotation)
-                .build()
-                // The analyzer can then be assigned to the instance
-                .also {
-                    it.setAnalyzer(Executors.newSingleThreadExecutor(), { luma ->
-                        // Values returned from our analyzer are passed to the attached listener
-                        // We log image analysis results here - you should do something useful
-                        // instead!
-                        Log.d(TAG, "Average luminosity: $luma")
-                    })
-                }
-
-            // Must unbind the use-cases before rebinding them
-            cameraProvider.unbindAll()
-
-            try {
-                // A variable number of use-cases can be passed here -
-                // camera provides access to CameraControl & CameraInfo
-                val camera = cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, imageAnalyzer
-                )
-
-                // Attach the viewfinder's surface provider to preview use case
-                preview.setSurfaceProvider(binding.pvFinder.surfaceProvider)
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
+            cameraProvider = cameraProviderFuture.get()
+            lensFacing = when {
+                hasBackCamera() -> CameraSelector.LENS_FACING_BACK
+                hasFrontCamera() -> CameraSelector.LENS_FACING_FRONT
+                else -> throw IllegalStateException("Back and front camera are unavailable")
             }
+            bindCameraUseCases()
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindCameraUseCases() {
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+        val rotation = binding.pvFinder.display.rotation
+        val cameraProvider = cameraProvider ?: return run {
+            Toast.makeText(this@ExampleCameraActivity, "Camera Setting Error", Toast.LENGTH_SHORT)
+                .show()
+            finish()
+        }
+        val preview = Preview.Builder()
+            .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            .setTargetRotation(rotation)
+            .build()
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            .setTargetRotation(rotation)
+            .build()
+        imageAnalyzer = ImageAnalysis.Builder()
+            .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            .setTargetRotation(rotation)
+            .build()
+            .also {
+                it.setAnalyzer(Executors.newSingleThreadExecutor(), { luma ->
+                    Log.d(TAG, "Average luminosity: $luma")
+                })
+            }
+        cameraProvider.unbindAll()
+        try {
+            cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                preview,
+                imageCapture,
+                imageAnalyzer
+            )
+            preview.setSurfaceProvider(binding.pvFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun hasBackCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) ?: false
+    }
+
+    private fun hasFrontCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
     }
 
     override fun onDestroy() {
