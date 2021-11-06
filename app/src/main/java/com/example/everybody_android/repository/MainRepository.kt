3174c36801e2a2c301350fd12ce8de118e979186
minjun8946 <minjun8946@daum.net>
2021-11-06 11:12:15 +0900

    delete repository

--- app/src/main/java/com/example/everybody_android/repository/MainRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.everybody_android.repository
-
-import com.example.everybody_android.dto.MainFeedData
-
-interface MainRepository {
-    suspend fun getMainFeed(gateWay : String) : List<MainFeedData>
-}
