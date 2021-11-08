c5a7a735d71b44fb72722f5b1b7f801b610ae0e4
minjun8946 <minjun8946@daum.net>
2021-11-08 16:11:56 +0900

    create folder activity

--- app/src/main/java/com/example/everybody_android/ui/CreateFolderActivity.kt
+++ app/src/main/java/com/example/everybody_android/ui/CreateFolderActivity.kt
@@ -2,11 +2,40 @@ package com.example.everybody_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
 import com.example.everybody_android.R
+import com.example.everybody_android.base.BaseActivity
+import com.example.everybody_android.databinding.ActivityCreateFolderBinding
+import com.example.everybody_android.viewmodel.CreateFolderViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class CreateFolderActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_folder)
+@AndroidEntryPoint
+class CreateFolderActivity : BaseActivity<ActivityCreateFolderBinding,CreateFolderViewModel>(){
+
+    override val layoutId: Int = R.layout.activity_create_folder
+    override val viewModel: CreateFolderViewModel = CreateFolderViewModel()
+
+    override fun init() {
+        edittextClick()
     }
+
+    private fun edittextClick(){
+        binding.etFolderName.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(binding.etFolderName.text.toString() != "" ) binding.ibSave.setImageDrawable(resources.getDrawable(R.drawable.ic_storage))
+                else binding.ibSave.setImageDrawable(resources.getDrawable(R.drawable.ic_frame_storage))
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+
+
+
 }
