d867fc870d7ed2d75a126740cfcba06ff0de8950
minjun8946 <minjun8946@daum.net>
2021-11-08 16:12:12 +0900

    main activity

--- app/src/main/java/com/example/everybody_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/everybody_android/ui/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.everybody_android.ui
 
+import android.content.Intent
+import android.os.Build
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.viewModels
 import com.example.everybody_android.R
 import com.example.everybody_android.base.BaseActivity
@@ -12,9 +16,37 @@ class MainActivity : BaseActivity<ActivityMainBinding,MainViewModel>() {
     override val layoutId = R.layout.activity_main
     override val viewModel: MainViewModel by viewModels()
 
+    var feedStatus = true
+
     override fun init() {
-        //viewModel.getMainFeed()
+        viewModel.testFeed()
+        intentCreateFolder()
+        feedSort()
+
     }
+
+    private fun intentCreateFolder(){
+        binding.ibAdd.setOnClickListener {
+            val intent = Intent(this,CreateFolderActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun feedSort(){
+        binding.ibSort.setOnClickListener {
+
+            if(feedStatus){
+                binding.rvFullSort.visibility = View.VISIBLE
+                binding.rvHalfSort.visibility = View.GONE
+            }
+            else{
+                binding.rvFullSort.visibility = View.GONE
+                binding.rvHalfSort.visibility = View.VISIBLE
+            }
+            feedStatus = !feedStatus
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/example/everybody_android/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/everybody_android/viewmodel/MainViewModel.kt
@@ -1,8 +1,14 @@
 package com.example.everybody_android.viewmodel
 
+import androidx.databinding.library.baseAdapters.BR
 import androidx.lifecycle.viewModelScope
+import com.example.everybody_android.R
+import com.example.everybody_android.adapter.RecyclerItem
+import com.example.everybody_android.adapter.RecyclerViewAdapter
 import com.example.everybody_android.base.BaseViewModel
 import com.example.everybody_android.di.ApiModule
+import com.example.everybody_android.dto.MainFeedData
+import com.example.everybody_android.dto.MainFeedPictureData
 import com.example.everybody_android.remote.ApiService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -12,17 +18,69 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-    private val apiService: ApiService
-) : BaseViewModel(){
-    val feedStatus = 0
+class MainViewModel @Inject constructor() : BaseViewModel() {
 
-    fun getMainFeed(){
-        viewModelScope.launch {
-            withContext(Dispatchers.IO){
-                val response = apiService.getMainFeed("")
-                println(response)
+    private var feedStatus = true
+    private var recyclerStatus = true
+
+    private var testFeedData = ArrayList<MainFeedData>()
+
+    val halfFeedAdapter = RecyclerViewAdapter()
+    val fullFeedAdapter = RecyclerViewAdapter()
+
+    fun sortFeed(){
+        recyclerStatus = !recyclerStatus
+        if (testFeedData.size == 0) feedStatus = false
+        else {
+            if (recyclerStatus) {
+                halfFeedAdapter.changeData(testFeedData.map { it.toHalfRecyclerItem() })
+                fullFeedAdapter.clearData()
+            }
+            else {
+                fullFeedAdapter.changeData(testFeedData.map { it.toFullRecyclerItem() })
+                halfFeedAdapter.clearData()
             }
         }
     }
+
+    fun testFeed() {
+        testFeedData = arrayListOf(
+            MainFeedData(
+                1, "피드이름", "YY.MM-YY.MM",
+                MainFeedPictureData(1, 1, "얼굴", "YY.MM-YY.MM", "", "", ""),
+            ),
+            MainFeedData(
+                1, "피드이름", "YY.MM-YY.MM",
+                MainFeedPictureData(1, 1, "얼굴", "YY.MM-YY.MM", "", "", "")
+            ),
+            MainFeedData(
+                1, "피드이름", "YY.MM-YY.MM",
+                MainFeedPictureData(1, 1, "얼굴", "YY.MM-YY.MM", "", "", "")
+            ),
+            MainFeedData(
+                1, "피드이름", "YY.MM-YY.MM",
+                MainFeedPictureData(1, 1, "얼굴", "YY.MM-YY.MM", "", "", "")
+            ),
+            MainFeedData(
+                1, "피드이름", "YY.MM-YY.MM",
+                MainFeedPictureData(1, 1, "얼굴", "YY.MM-YY.MM", "", "", "")
+            )
+        )
+        sortFeed()
+    }
+
+
+    private fun MainFeedData.toFullRecyclerItem() =
+        RecyclerItem(
+            data = this,
+            variableId = BR.repo,
+            layoutId = R.layout.item_full_mainfeed
+        )
+
+    private fun MainFeedData.toHalfRecyclerItem() =
+        RecyclerItem(
+            data = this,
+            variableId = BR.repo,
+            layoutId = R.layout.item_half_mainfeed
+        )
 }
